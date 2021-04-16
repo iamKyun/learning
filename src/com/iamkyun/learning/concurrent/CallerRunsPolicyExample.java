d480784313432941d2b9b3385f0dd16d930764a3
Kyun <kyun@iamkyun.com>
2021-04-16 16:10:18 +0800

    update

---
+++ src/com/iamkyun/learning/concurrent/CallerRunsPolicyExample.java
@@ -0,0 +1,70 @@
+package com.iamkyun.learning.concurrent;
+
+import java.time.Instant;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CallerRunsPolicyExample {
+    private static final int CORE_POOL_SIZE = 5;
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int QUEUE_CAPACITY = 10;
+    private static final Long KEEP_ALIVE_TIME = 1L;
+
+    public static void main(String[] args) {
+
+        //使用阿里巴巴推荐的创建线程池的方式
+        //通过ThreadPoolExecutor构造函数自定义参数创建
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+                new NamedThreadFactory("线程池"),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            int size = executor.getQueue().size();
+            System.out.println(size);
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(
+                        "Task" + finalI + " CurrentThread name:" + Thread.currentThread().getName() + "date：" +
+                        Instant.now());
+            });
+        }
+        //终止线程池
+        executor.shutdown();
+        try {
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished all threads");
+    }
+
+    public static class NamedThreadFactory implements ThreadFactory {
+        private final AtomicInteger sequence = new AtomicInteger(1);
+        private final String prefix;
+
+        public NamedThreadFactory(String prefix) {
+            this.prefix = prefix;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            int seq = sequence.getAndIncrement();
+            thread.setName(prefix + (seq > 1 ? "-" + seq : ""));
+            return thread;
+        }
+    }
+}

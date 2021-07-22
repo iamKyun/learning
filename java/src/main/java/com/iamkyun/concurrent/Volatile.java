509cf860f558f0c9f657b2a19fadee9cc1387ca4
Kyun <kyun@iamkyun.com>
2021-07-22 16:47:54 +0800

    volatile

---
+++ java/src/main/java/com/iamkyun/concurrent/Volatile.java
@@ -0,0 +1,49 @@
+package com.iamkyun.concurrent;
+
+public class Volatile {
+    // private static int  sCount = 0;
+    private volatile static int sCount = 0;
+
+    public static void main(String[] args) {
+        new Consumer().start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            return;
+        }
+        new Producer().start();
+    }
+
+    static class Consumer extends Thread {
+        @Override
+        public void run() {
+            int localValue = -1;
+            while (true) {
+                if (localValue != sCount) {
+                    System.out.println("Consumer: detected count change " + sCount);
+                    localValue = sCount;
+                }
+                if (sCount >= 5) {
+                    break;
+                }
+            }
+            System.out.println("Consumer: terminating");
+        }
+    }
+
+    static class Producer extends Thread {
+        @Override
+        public void run() {
+            while (sCount < 5) {
+                sCount++;
+                System.out.println("Producer: incrementing count to " + sCount);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+            System.out.println("Producer: terminating");
+        }
+    }
+}

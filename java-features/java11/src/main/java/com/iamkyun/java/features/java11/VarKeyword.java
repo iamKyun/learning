2a6619b6a1aab5c3909b1870b6f052a1aa66d330
Kyun <kyun@iamkyun.com>
2021-12-20 16:41:40 +0800

    demo

---
+++ java-features/java11/src/main/java/com/iamkyun/java/features/java11/VarKeyword.java
@@ -0,0 +1,8 @@
+package com.iamkyun.java.features.java11;
+
+public class VarKeyword {
+    public static void main(String[] args) {
+        var greeting = "Hello World!";
+        System.out.println(greeting);
+    }
+}
